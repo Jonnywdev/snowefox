83510e0b56dca49f33680319e58a1cfb79ac9f88
Jonnywdev <jonnywhitt19@gmail.com>
2022-11-15 15:20:26 +0000

    Added products app

---
+++ products/product_choices.py
@@ -0,0 +1,12 @@
+"""
+Sizes for products/models.py
+"""
+SIZE_CHOICES = (
+    ('xs', 'XS'),
+    ('s', 'S'),
+    ('m', 'M'),
+    ('l', 'L'),
+    ('xl', 'XL'),
+    ('xxl', 'XXL'),
+    ('xxxl', 'XXXL'),
+)
