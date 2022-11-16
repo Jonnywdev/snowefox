172816b7cec57c273ef1b3050360269fc746ce74
Jonnywdev <jonnywhitt19@gmail.com>
2022-11-16 12:56:22 +0000

    Remove evenything from the dtatabase, test to fix a bug - unsucessfull

--- products/models.py
+++ products/models.py
@@ -1,26 +1,7 @@
 from django.db import models
 from .product_choices import SIZE_CHOICES
+from clubs.models import Club
 # Create your models here.
-
-
-class Club(models.Model):
-    class Meta:
-
-        verbose_name_plural = 'Clubs'
-
-    name = models.CharField(max_length=254, unique=True)
-    friendly_name = models.CharField(max_length=254, unique=True)
-    sport = models.CharField(max_length=60, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-
-        return f'{self.name}'
-
-    def get_friendly_name(self):
-
-        return self.friendly_name
 
 
 class Category(models.Model):
@@ -41,7 +22,8 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
-    club = models.ForeignKey('Club', null=True, blank=True,
+    club = models.ForeignKey('clubs.Club', verbose_name='Club',
+                             related_name='clubs', null=True, blank=True,
                              on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
