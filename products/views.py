f80a5396bb23016f5b5e1bd1c518f9d33230ca23
Jonnywdev <jonnywhitt19@gmail.com>
2022-11-25 10:59:42 +0000

    search functionality working

--- products/views.py
+++ products/views.py
@@ -13,12 +13,12 @@ def all_products(request):
 
     if request.GET:
         if 'q' in request.GET:
-            query = request.Get['q']
+            query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name_icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
